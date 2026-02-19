d2f3be4159f62ffb1f090cad4969fb4206dd69d7
gpreddy817 <girishd0817@gmail.com>
2026-02-19 14:36:21 +0530

    feat(models): create post model schema

---
+++ Backend/src/models/post.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    caption: {
+        type: String,
+        default: ""
+    },
+
+    imgUrl:{ 
+        type: String,
+        required: [true, "imgUrl is required for creating a post"]
+    },
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: [true, "userid is required to create a post"]
+    },
+
+    likes:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }
+ 
+})
+
+const postModel = mongoose.model("posts", postSchema)
+
+module.exports = postModel
