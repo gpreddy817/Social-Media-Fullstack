7372cd059bb3c699683a645344fee395a7157a44
gpreddy817 <girishd0817@gmail.com>
2026-02-19 14:37:03 +0530

    feat(routes): add post routes for CRUD operations

---
+++ Backend/src/routes/post.routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const postRouter = express.Router()
+const postcontroller = require("../controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ Storage: multer.memoryStorage() })
+const identifyuser = require("../middlewares/auth.middleware")
+
+postRouter.post("/", upload.single("image") , identifyuser ,postcontroller.CreatePostController)
+
+postRouter.get("/", identifyuser, postcontroller.getPostController)
+
+postRouter.get("/details/:postId", identifyuser, postcontroller.getPostDetailsController)
+
+postRouter.post("/like/:postId", identifyuser, postcontroller.likeUserController)
+
+module.exports = postRouter
