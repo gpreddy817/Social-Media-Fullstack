521558804d66b0bfa192aaeac4b0b28312444e67
gpreddy817 <girishd0817@gmail.com>
2026-02-25 22:08:30 +0530

    "Update auth context to provide current user data for post authorship."

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,20 +1,19 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 
-export const AuthContext = createContext();
+
+export const AuthContext = createContext()
+
 
 export const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [ user, setUser ] = useState(null)
+    const [ loading, setLoading ] = useState(false)
 
+    
     return (
-        <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
+        <AuthContext.Provider value={{ user, setUser, loading, setLoading }} >
             {children}
         </AuthContext.Provider>
-    );
-};
+    )
 
-// optional but recommended
-export const useAuth = () => {
-    return useContext(AuthContext);
-};
+}
